1287bb2ef0d7f59558e1563515f8db9a6356801a
henriqueramos23 <131548572+henriqueramos23@users.noreply.github.com>
2023-08-09 12:07:16 -0300

    Upload site Iphone14

---
+++ scipts.js
@@ -0,0 +1,11 @@
+function trocaCor(cor) {
+    let circulo = document.querySelector(".circulo")
+    console.log(circulo)
+    circulo.style.background = cor
+}
+function trocaImagem(imagem) {
+    let Iphone = document.querySelector(".iphone")
+    Iphone.src = imagem
+    console.log(imgIphone)
+    
+}
